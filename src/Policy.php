e619dbfbb63d0a29a2dd14948502e49c4ce2fe26
atendwa <atendwa@gmail.com>
2025-06-01 20:45:02 +0300

    chore(policies): updates policy class

--- src/Policy.php
+++ src/Policy.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Atendwa\Support;
 
 use Atendwa\Support\Concerns\Support\UsesPolicySetup;
-use Illuminate\Auth\Access\HandlesAuthorization;
 
 class Policy
 {
-    use HandlesAuthorization;
     use UsesPolicySetup;
 
     protected ?string $resource = null;
