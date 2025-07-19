5be0e611bb10a7cf55c7683e87c0c3bed76c741c
atendwa <atendwa@gmail.com>
2025-07-20 00:00:13 +0300

    chore: minor updates

--- src/Job.php
+++ src/Job.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Atendwa\Support;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class Job implements ShouldQueue
+class Job implements ShouldBeEncrypted, ShouldQueue
 {
     use Dispatchable;
     use InteractsWithQueue;
